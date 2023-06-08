8c3f6c9b131caad3a0f52f66c0c61e4bef632651
Kim JinWoo <kimjinu2540@gmail.com>
2023-06-08 18:48:05 +0900

    header, footer 고정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -6,17 +6,31 @@ import Cart from "./pages/Cart";
 import MyPage from "./pages/MyPage";
 import Payment from "./pages/Payment";
 import Sign from "./pages/Sign";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+};
 
 function App() {
     return (
         <div className="app">
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="mypage" element={<MyPage />} />
-                <Route path="payment" element={<Payment />} />
-                <Route path="sign" element={<Sign />} />
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="mypage" element={<MyPage />} />
+                    <Route path="payment" element={<Payment />} />
+                    <Route path="sign" element={<Sign />} />
+                </Route>
             </Routes>
         </div>
     );
