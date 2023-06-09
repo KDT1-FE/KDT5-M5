5cdd449dfe14565ff656219ba3fbefe5b0414b59
BearHumanS <bye0651@naver.com>
2023-06-09 21:31:51 +0900

    필요없는 부분 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,59 @@
-import { Outlet, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
-import MyPage from "./pages/MyPage";
-import Payment from "./pages/Payment";
-import Sign from "./pages/Sign";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Test from "../Test"
+import { Outlet, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
+import MyPage from './pages/MyPage'
+import Payment from './pages/Payment'
+import Sign from './pages/Sign'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const Layout = () => {
-    return (
-        <div>
-            <Header />
-            <Outlet />
-            <Footer />
-        </div>
-    );
-};
-
-function App() {
-    return (
-        <div className="app">
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="/cart" element={<Test />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="mypage" element={<MyPage />} />
-                    <Route path="payment" element={<Payment />} />
-                    <Route path="sign" element={<Sign />} />
-                </Route>
-            </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
 }
 
-export default App;
+function App() {
+  return (
+    <div className="app">
+      <Routes>
+        <Route
+          path="/"
+          element={<Layout />}>
+          <Route
+            index
+            element={<Home />}
+          />
+          <Route
+            path="/cart"
+            element={<Cart />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            path="mypage"
+            element={<MyPage />}
+          />
+          <Route
+            path="payment"
+            element={<Payment />}
+          />
+          <Route
+            path="sign"
+            element={<Sign />}
+          />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
