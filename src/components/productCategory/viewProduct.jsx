24d6821b5d841bf66e4255959a59cd52a2eaacff
BearHumanS <bye0651@naver.com>
2023-06-30 16:26:45 +0900

    카테고리별 제품 페이지 가격순 정리 및 css수정

--- src/components/productCategory/viewProduct.jsx
+++ src/components/productCategory/viewProduct.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { getProducts } from '../../store/AdminAPI'
 import ProductList from '../main/ProductList'
 import { Link, useParams } from 'react-router-dom'
+import './viewProduct.css'
 
 const ViewProduct = () => {
   const [products, setProducts] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [isAscendingOrder, setIsAscendingOrder] = useState(true)
   const { category } = useParams()
 
   useEffect(() => {
@@ -17,32 +19,63 @@ const ViewProduct = () => {
         productsCategory = res.filter(product => product.tags[0] === category)
       }
 
-      setProducts(productsCategory)
+      const sortedProducts = [...productsCategory].sort((a, b) => {
+        if (isAscendingOrder) {
+          return a.price - b.price
+        } else {
+          return b.price - a.price
+        }
+      })
+
+      setProducts(sortedProducts)
       setIsLoading(false)
     }
 
     productData()
-  }, [category])
+  }, [category, isAscendingOrder])
+
+  const handleLowPriceClick = () => {
+    setIsAscendingOrder(true)
+  }
+
+  const handleHighPriceClick = () => {
+    setIsAscendingOrder(false)
+  }
 
   if (isLoading) {
     return <div>로딩 중...</div>
   }
 
   return (
-    <div className="product-container view">
-      {products.map(product => (
-        <Link
-          to={`/product/${product.tags[0]}/${product.id}`}
-          key={product.id}>
-          <ProductList
-            id={product.id}
-            title={product.title}
-            price={product.price}
-            thumbnail={product.thumbnail}
-          />
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className="price-sort">
+        <span
+          className={isAscendingOrder ? 'active' : ''}
+          onClick={handleLowPriceClick}>
+          낮은 가격순
+        </span>{' '}
+        <span> &nbsp;|&nbsp; </span>
+        <span
+          className={!isAscendingOrder ? 'active' : ''}
+          onClick={handleHighPriceClick}>
+          높은 가격순
+        </span>
+      </div>
+      <div className="product-container view">
+        {products.map(product => (
+          <Link
+            to={`/product/${product.tags[0]}/${product.id}`}
+            key={product.id}>
+            <ProductList
+              id={product.id}
+              title={product.title}
+              price={product.price}
+              thumbnail={product.thumbnail}
+            />
+          </Link>
+        ))}
+      </div>
+    </>
   )
 }
 
