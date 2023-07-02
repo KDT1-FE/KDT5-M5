11f79fd12155b97e5455e1c171747924842fe296
BearHumanS <bye0651@naver.com>
2023-07-03 00:46:23 +0900

    이미지 url로 수정

--- src/components/main/Banner.jsx
+++ src/components/main/Banner.jsx
@@ -36,19 +36,19 @@ const Banner = () => {
           {...settings}>
           <div>
             <img
-              src="../../public/productEffective/cleansing_effect.jpg"
+              src="https://github.com/BearHumanS/Frontend_Team-project/assets/115094069/3d66cd2d-4a51-47ee-b50c-274ecb8dd22a"
               alt="클렌징 제품 효과"
             />
           </div>
           <div>
             <img
-              src="../../public/productEffective/makeup_effect.jpg"
+              src="https://github.com/BearHumanS/Frontend_Team-project/assets/115094069/34e7aaa2-9645-4a25-86f7-6ad15a3e43cd"
               alt="메이크업 제품 효과"
             />
           </div>
           <div>
             <img
-              src="../../public/productEffective/skincare_effect.jpg"
+              src="https://github.com/BearHumanS/Frontend_Team-project/assets/115094069/65d8c8f1-6859-479c-b248-defc6286662b"
               alt="스킨케어 제품 효과"
             />
           </div>
