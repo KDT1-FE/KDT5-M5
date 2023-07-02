9b143d3864a2131b1f8c4c01ff83783e2bda7f4b
Kim JinWoo <kimjinu2540@gmail.com>
2023-07-03 01:43:07 +0900

    수정 완료

--- src/components/myPage/getOrderList/OrderCancel.jsx
+++ src/components/myPage/getOrderList/OrderCancel.jsx
@@ -4,8 +4,9 @@ import { cancelProducts } from '../../../store/ProductTransactions'
 const OrderCancel = ({ detailId }) => {
   const cancel = async () => {
     const res = await cancelProducts(detailId)
-    console.log(res)
+
     alert(res)
+    location.reload()
   }
 
   return <button onClick={cancel}>구매취소</button>
--- src/components/myPage/getOrderList/OrderOk.jsx
+++ src/components/myPage/getOrderList/OrderOk.jsx
@@ -4,8 +4,8 @@ import { confirm } from '../../../store/ProductTransactions'
 const OrderOk = ({ detailId }) => {
   const cancel = async () => {
     const res = await confirm(detailId)
-    console.log(res)
     alert(res)
+    location.reload()
   }
 
   return <button onClick={cancel}>구매확정</button>
